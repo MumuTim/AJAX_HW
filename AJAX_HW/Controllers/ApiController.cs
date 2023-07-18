74e2c3fe3a5c73af0adab96ac0674c6e8b5e3aa8
MuMu <tim80031@hotmail.com>
2023-07-19 01:04:55 +0800

    加入專案檔案。

---
+++ AJAX_HW/Controllers/ApiController.cs
@@ -0,0 +1,41 @@
+﻿using AJAX_HW.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AJAX_HW.Controllers
+{
+    public class ApiController : Controller
+    {
+        private DemoContext _context;
+
+        public ApiController(DemoContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Cities()
+        {
+            var cities = _context.Address.Select(x => x.City).Distinct();
+
+            return Json(cities);
+        }
+
+        public IActionResult Districts(string city)
+        {
+            var districts = _context.Address.Where(x => x.City == city).Select(x => x.SiteId).Distinct();
+
+            return Json(districts);
+        }
+
+        public IActionResult Roads(string district)
+        {
+            var roads = _context.Address.Where(x => x.SiteId == district).Select(x => x.Road).Distinct();
+
+            return Json(roads);
+        }
+    }
+}
---
+++ AJAX_HW/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using AJAX_HW.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace AJAX_HW.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Hw1()
+        {
+            return View();
+        }
+
+        public IActionResult Hw2()
+        {
+            return View();
+        }
+
+        public IActionResult Hw3()
+        {
+            return View();
+        }
+
+        public IActionResult Hw5()
+        {
+            return View();
+        }
+
+        public IActionResult Hw6()
+        {
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
